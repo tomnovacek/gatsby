041a6d3ed5b62b3b94ddc5d6080664b87abf6136
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-06-04 17:35:17 +0200

    write modules used by page queries to page-data files

--- packages/gatsby/src/utils/page-data.ts
+++ packages/gatsby/src/utils/page-data.ts
@@ -10,6 +10,7 @@ import { IExecutionResult } from "../query/types"
 interface IPageData {
   componentChunkName: IGatsbyPage["componentChunkName"]
   matchPath?: IGatsbyPage["matchPath"]
+  moduleDependencies: string[]
   path: IGatsbyPage["path"]
 }
 
@@ -55,7 +56,12 @@ export async function removePageData(
 
 export async function writePageData(
   publicDir: string,
-  { componentChunkName, matchPath, path: pagePath }: IPageData
+  {
+    componentChunkName,
+    matchPath,
+    moduleDependencies,
+    path: pagePath,
+  }: IPageData
 ): Promise<IPageDataWithQueryResult> {
   const inputFilePath = path.join(
     publicDir,
@@ -71,6 +77,7 @@ export async function writePageData(
     path: pagePath,
     matchPath,
     result,
+    moduleDependencies,
   }
   const bodyStr = JSON.stringify(body)
   // transform asset size to kB (from bytes) to fit 64 bit to numbers
@@ -102,7 +109,13 @@ export async function flush(): Promise<void> {
   }
   isFlushPending = false
   isFlushing = true
-  const { pendingPageDataWrites, components, pages, program } = store.getState()
+  const {
+    pendingPageDataWrites,
+    components,
+    pages,
+    program,
+    queryModuleDependencies,
+  } = store.getState()
 
   const { pagePaths, templatePaths } = pendingPageDataWrites
 
@@ -127,10 +140,14 @@ export async function flush(): Promise<void> {
     // them, a page might not exist anymore щ（ﾟДﾟщ）
     // This is why we need this check
     if (page) {
-      const body = await writePageData(
-        path.join(program.directory, `public`),
-        page
+      const moduleDependencies = Array.from(
+        queryModuleDependencies.get(pagePath) || []
       )
+
+      const body = await writePageData(path.join(program.directory, `public`), {
+        ...page,
+        moduleDependencies,
+      })
 
       if (program?._?.[0] === `develop`) {
         websocketManager.emitPageData({
